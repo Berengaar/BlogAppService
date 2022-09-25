5d940f7a1ba90c0d9308d0c600c81a6d9e4c85a1
Berengaar <emir97han@gmail.com>
2022-09-25 17:41:27 +0300

    Abstract repositories created

---
+++ src/BlogAppService.Application/Common/Repositories/IReadRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogAppService.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppService.Application.Common.Repositories
+{
+    public interface IReadRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        Task<IList<T>> GetAllAsync();
+        Task<IList<T>> GetWhereAsync(Expression<Func<T, bool>> predicate);
+        Task<T> FindAsync(Expression<Func<T, bool>> predicate);
+    }
+}
