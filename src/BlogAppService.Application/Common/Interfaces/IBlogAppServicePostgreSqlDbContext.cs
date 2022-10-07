2936bf6a49d0677157a651ec0b8f1679953aee45
Berengaar <emir97han@gmail.com>
2022-10-07 18:26:50 +0300

    DbContext objects created

---
+++ src/BlogAppService.Application/Common/Interfaces/IBlogAppServicePostgreSqlDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BlogAppService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppService.Application.Common.Interfaces
+{
+    public interface IBlogAppServicePostgreSqlDbContext
+    {
+        DbSet<Article> Articles { get; set; }
+        DbSet<Comment> Comments { get; set; }
+        DbSet<Category> Categories { get; set; }
+        DbSet<Relish> Relishes { get; set; }
+    }
+}
---
+++ src/BlogAppService.Infrastructure/Persistance/Contexts/BlogAppServicePostgreSqlDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BlogAppService.Application.Common.Interfaces;
+using BlogAppService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppService.Infrastructure.Persistance.Contexts
+{
+    public class BlogAppServicePostgreSqlDbContext : IBlogAppServicePostgreSqlDbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Relish> Relishes { get; set; }
+    }
+}
