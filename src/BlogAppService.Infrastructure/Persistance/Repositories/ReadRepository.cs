c455c9bd91aa62ec396da4c0ba6f6e7527706082
Berengaar <emir97han@gmail.com>
2022-10-07 18:26:20 +0300

    Repositories created

---
+++ src/BlogAppService.Infrastructure/Persistance/Repositories/ReadRepository.cs
@@ -0,0 +1,32 @@
+﻿using BlogAppService.Application.Common.Repositories;
+using BlogAppService.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppService.Infrastructure.Persistance.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : BaseEntity
+    {
+        public DbSet<T> Table => throw new NotImplementedException();
+
+        public Task<T> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetWhereAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/BlogAppService.Infrastructure/Persistance/Repositories/WriteRepository.cs
@@ -0,0 +1,31 @@
+﻿using BlogAppService.Application.Common.Repositories;
+using BlogAppService.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppService.Infrastructure.Persistance.Repositories
+{
+    public class WriteRepository<T> : IWriteRepository<T> where T : BaseEntity
+    {
+        public DbSet<T> Table => throw new NotImplementedException();
+
+        public Task<bool> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
