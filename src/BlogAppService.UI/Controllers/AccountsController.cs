682ba40493c412b90aaa5508063ccaeb2e80ee06
Berengaar <emir97han@gmail.com>
2022-10-12 02:06:12 +0300

    Account Controller created and tried {Success}

---
+++ src/BlogAppService.UI/Controllers/AccountsController.cs
@@ -0,0 +1,50 @@
+﻿using BlogAppService.Application.Common.Interfaces;
+using BlogAppService.Application.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogAppService.UI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+
+        public AccountsController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel user)
+        {
+            var result = await _identityService.RegisterAsync(user);
+            if (result.Succeeded)
+            {
+                return Created("~api/Accounts", user);
+            }
+            else if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+            else
+            {
+                return StatusCode(501);
+            }
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel user)
+        {
+            var result = await _identityService.LoginAsync(user);
+            if (result.Item1.Succeeded)
+            {
+                Response.Headers.Add("JWT", result.token);
+                return Ok();
+            }
+            else
+            {
+                return BadRequest(result.Item1.Errors);
+            }
+        }
+    }
+}
