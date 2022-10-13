93f72a65a27d4857fb1560f50843d172ef5f838e
Berengaar <emir97han@gmail.com>
2022-10-14 00:45:09 +0300

    Unit of work added to project

---
+++ src/BlogAppService.Application/Common/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using BlogAppService.Application.Common.Repositories.ArticleCommentRepository;
+using BlogAppService.Application.Common.Repositories.ArticleRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppService.Application.Common.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IArticleReadRepository ArticleReadRepository { get; }
+        IArticleWriteRepository ArticleWriteRepository { get; }
+        IArticleCommentReadRepository ArticleCommentReadRepository { get; }
+        IArticleCommentWriteRepository ArticleCommentWriteRepository { get; }
+    }
+}
---
+++ src/BlogAppService.Infrastructure/Persistance/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using BlogAppService.Application.Common.Interfaces;
+using BlogAppService.Application.Common.Repositories.ArticleCommentRepository;
+using BlogAppService.Application.Common.Repositories.ArticleRepository;
+using BlogAppService.Infrastructure.Persistance.Contexts;
+using BlogAppService.Infrastructure.Persistance.Repositories.ArticleCommentRepository;
+using BlogAppService.Infrastructure.Persistance.Repositories.ArticleRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppService.Infrastructure.Persistance
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly BlogAppServicePostgreSqlDbContext _context;
+        private ArticleReadRepository _articleReadRepository;
+        private ArticleWriteRepository _articleWriteRepository;
+        private ArticleCommentReadRepository _articleCommentReadRepository;
+        private ArticleCommentWriteRepository _articleCommentWriteRepository;
+        public UnitOfWork(ArticleReadRepository articleReadRepository, ArticleWriteRepository articleWriteRepository, ArticleCommentReadRepository articleCommentReadRepository, ArticleCommentWriteRepository articleCommentWriteRepository, BlogAppServicePostgreSqlDbContext context)
+        {
+            _articleReadRepository = articleReadRepository;
+            _articleWriteRepository = articleWriteRepository;
+            _articleCommentReadRepository = articleCommentReadRepository;
+            _articleCommentWriteRepository = articleCommentWriteRepository;
+            _context = context;
+        }
+        public IArticleReadRepository ArticleReadRepository => _articleReadRepository ?? (_articleReadRepository = new ArticleReadRepository(_context));
+
+        public IArticleWriteRepository ArticleWriteRepository => _articleWriteRepository ?? (_articleWriteRepository = new ArticleWriteRepository(_context));
+
+        public IArticleCommentReadRepository ArticleCommentReadRepository => _articleCommentReadRepository ?? (_articleCommentReadRepository = new ArticleCommentReadRepository(_context));
+
+        public IArticleCommentWriteRepository ArticleCommentWriteRepository => _articleCommentWriteRepository ?? (_articleCommentWriteRepository = new ArticleCommentWriteRepository(_context));
+    }
+}
